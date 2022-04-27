9d54e3df6fccc39c1c25dcfc1357a1707e6c5096
Rupali Natewad <rupalirnatewad@gmail.com>
2022-04-27 23:51:25 +0530

    uploading Emp_uc7 file

---
+++ Emp_uc7.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpLine.prb
+{
+    internal class Emp_uc7
+    {
+        public const int Is_Full_Time = 1;
+        public const int Is_Part_Time = 2;
+        public const int Emp_Rate_Per_Hour = 20;
+        public const int Num_Of_Working_Days = 20;
+        public const int Max_Hours_In_Month = 100;
+        public  static int EmpMax100HrsWage()
+        {
+            int totalWorkingDays = 0;
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+
+            while (totalEmpHrs <= Max_Hours_In_Month && totalWorkingDays < Num_Of_Working_Days)
+            {
+                totalWorkingDays++;
+                Random obj = new Random();
+                int num = obj.Next(0, 3);
+                switch (num)
+                {
+                    case Is_Full_Time:
+                        empHrs = 8;
+                        break;
+
+                    case Is_Part_Time:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : {0} Emp Hours : {1}", totalWorkingDays, empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Total Employee Wage: " + totalEmpWage);
+
+            return totalEmpWage;
+        }
+       
+
+    }
+}
